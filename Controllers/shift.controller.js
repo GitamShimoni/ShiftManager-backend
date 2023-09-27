75b1035d3ee0682e748f1ee20ba452ff2fa6ddc0
ofek15 <segal.ofek@ofek.com>
2023-09-27 13:31:11 +0300

    blabla

--- Controllers/shift.controller.js
+++ Controllers/shift.controller.js
@@ -10,6 +10,42 @@ const fetchAllShifts = async (req, res) => {
   }
 };
 
+const CreateShiftRequest = async (req, res) => {
+  console.log("hii")
+  const {
+    day,
+    endDate,
+    endShiftHour,
+    endShiftMinutes,
+    hour,
+    id,
+    startDate,
+    startShiftHour,
+    startShiftMinutes,
+    title
+  } = req.body;
+  try {
+    const newShift = await Shift.create({
+      day,
+      endDate,
+      endShiftHour,
+      endShiftMinutes,
+      hour,
+      id,
+      startDate,
+      startShiftHour,
+      startShiftMinutes,
+      status:"accept",
+      title
+    });
+    console.log(newShift)
+    return res.status(200).json(newShift);
+  } catch (err) {
+    res.status(500).json(err + "Shift Could Not Be Created!");
+  }
+};
+
 module.exports = {
   fetchAllShifts,
+  CreateShiftRequest
 };
--- Routes/shift.route.js
+++ Routes/shift.route.js
@@ -4,6 +4,7 @@ const express = require("express");
 const router = express.Router();
 
 router.route("/fetchAllShifts").get(shiftService.fetchAllShifts);
+router.route("/CreateShiftRequest").post(shiftService.CreateShiftRequest);
 
 // router.route("/getUser").post(userService.getUser);
 
